d1598828a1b9e71a3907350beff7a7d6cef3e81a
iimoiseev <vanish8989@yandex.ru>
2022-06-29 19:31:59 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../assets/styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -13,9 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">111!</a>
         </h1>
-
+        <p className="material-icons">pie_chart</p>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -65,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
